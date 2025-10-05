67eb809d709ffc1ada026ae375fd0f09c58f9759
Cheryn <cherynpink@gmail.com>
2025-10-05 16:51:18 -0400

    ejs blogs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
     const express = require('express');
     const app = express();
     const port = 3000; // Or any desired port
+    const path = require('path'); // Required for path.join
 
     // Serve static files (HTML, CSS, images, JS) from a 'public' directory
     app.use(express.static('public'));
@@ -14,3 +15,10 @@
     app.listen(port, () => {
       console.log(`Server listening at http://localhost:${port}`);
     });
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Define the directory where your EJS files (views) are located
+// By default, Express looks for a 'views' directory in the root of your project
+app.set('views', path.join(__dirname, 'views'));
