dca3cdbdb409519ba3743f02d90eafab155a8189
Cheryn <cherynpink@gmail.com>
2025-11-30 20:09:23 -0500

    Changed Answers

--- public/questions.js
+++ public/questions.js
@@ -1,75 +1,75 @@
 let x01 = {question : "The first large group of Korean immigrants arrived in the US in 1903 to work mainly on which US territory's plantations?",
-    A : "Florida",
+answers : {A : "Florida",
     B : "California",
     C : "Texas",
     D : "Hawaii"},
 correct : "D"}
 
 let x02 = {question : "In 1913, Korean immigrants established a farming community called "Pachappa Camp," considered the first Korean settlement in the mainland US. Where was it located?",
-    A : "Los Angeles, CA",
+answers : {A : "Los Angeles, CA",
     B : "San Francisco, CA",
     C : "Riverside, CA",
     D : "Portland, Oregon"},
 correct : "C"}
 
 let x03 = {question : "What US law in 1924 effectively stopped Korean immigration until after World War II?",
-    A : "Immigration and Nationality Act",
+answers : {A : "Immigration and Nationality Act",
     B : "Chinese Exclusion Act",
     C : "Immigration Act of 1924",
     D : "Gentlemen's Agreement"},
 correct : "C"}
 
 let x04 = {question : "The Korean War led to a new wave of Korean immigration. Which US law allowed Korean War brides and orphans to immigrate to the US?",
-    A : "Refugee Act",
+answers : {A : "Refugee Act",
     B : "War Brides Act",
     C : "Civil Rights Act",
     D : "McCarran-Walter Act"},
 correct : "C"}
 
 let x05 = {question : "What U.S. law in 1965 allowed a new wave of Korean immigrants to enter the country?",
-    A : "Mccarran-Walter Act",
+answers : {A : "Mccarran-Walter Act",
     B : "Civil Rights Act",
     C : "Immigration and Nationality Act",
     D : "Asian American Fair Immigration Act"},
 correct : "C"}
 
 let x06 = {question : "What event in Los Angeles in 1992 deeply affected the Korean American community, especially shop owners?",
-    A : "Korean War veterans' protests",
+answers : {A : "Korean War veterans' protests",
     B : "Immigration reform marches",
     C : "Rodney King uprisings (LA riots)",
     D : "Watts Riots"},
 correct : "C"}
 
 let x07 = {question : "Which U.S. city has the largest Korean American community, known as Koreatown?",
-    A : "Seattle",
+answers : {A : "Seattle",
     B : "San Francisco",
     C : "Los Angeles",
     D : "New York City"},
 correct : "C"}
 
 let x08 = {question : "What event in Los Angeles in 1992 deeply affected the Korean American community, especially shop owners?",
-    A : "Korean War veterans' protests",
+answers : {A : "Korean War veterans' protests",
     B : "Immigration reform marches",
     C : "Rodney King uprisings (LA riots)",
     D : "Watts Riots"},
 correct : "C"}
 
 let x09 = {question : "What event in Los Angeles in 1992 deeply affected the Korean American community, especially shop owners?",
-    A : "Korean War veterans' protests",
+answers : {A : "Korean War veterans' protests",
     B : "Immigration reform marches",
     C : "Rodney King uprisings (LA riots)",
     D : "Watts Riots"},
 correct : "C"}
 
 let x10 = {question : "What event in Los Angeles in 1992 deeply affected the Korean American community, especially shop owners?",
-    A : "Korean War veterans' protests",
+answers : {A : "Korean War veterans' protests",
     B : "Immigration reform marches",
     C : "Rodney King uprisings (LA riots)",
     D : "Watts Riots"},
 correct : "C"}
 
 let x11 = {question : "What event in Los Angeles in 1992 deeply affected the Korean American community, especially shop owners?",
-    A : "Korean War veterans' protests",
+answers : {A : "Korean War veterans' protests",
     B : "Immigration reform marches",
     C : "Rodney King uprisings (LA riots)",
     D : "Watts Riots"},
